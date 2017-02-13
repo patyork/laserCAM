aac05a14d178e147ac1a595a22cbbb79ac1c88bc
Pat York <guitarp_11@yahoo.com>
2017-02-13 14:20:29 -0800

    flask intial test

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, send_from_directory
+import json
 app = Flask(__name__, static_url_path='')
 
 app._static_folder = ''
@@ -6,6 +7,15 @@ app._static_folder = ''
 @app.route("/")
 def hello():
     return app.send_static_file('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print 'hey, here'
+        print request
+        return json.dumps([{'hey2': 'hey'}])
+    return json.dumps([{'hey': 'hey'}])
 
 if __name__ == "__main__":
     app.run()
